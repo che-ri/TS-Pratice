6efe487d5104954ce5dfee4e91d5abb490e25645
che-ri <rnjs1135@gmail.com>
2021-09-28 22:37:46 +0900

    옵셔널한 props의 타입 처리하기

--- ts-react-tutorial/src/Greetings.tsx
+++ ts-react-tutorial/src/Greetings.tsx
@@ -3,15 +3,17 @@ import React from "react";
 type GreetingsProps = {
     name: string;
     mark: string;
+    optional?: string; //옵셔널한 props라면 ? 문자를 사용하면 됩니다.
 };
 
-const Greetings = ({ name, mark }: GreetingsProps) => {
+function Greetings({ name, mark, optional }: GreetingsProps) {
     return (
         <div>
             Hello, {name} {mark}
+            {optional && <p>{optional}</p>}
         </div>
     );
-};
+}
 
 Greetings.defaultProps = {
     mark: "!",
