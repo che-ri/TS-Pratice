11268a1a78b221e58f61f587a2c86c002621b4fc
che-ri <rnjs1135@gmail.com>
2021-10-12 03:17:26 +0900

    typesafe-action으로 Counter모듈 리팩토링하기

--- ts-react-tutorial/src/modules/counter.ts
+++ ts-react-tutorial/src/modules/counter.ts
@@ -1,28 +1,23 @@
+import { ActionType, createReducer, deprecated } from "typesafe-actions";
+const { createStandardAction } = deprecated;
+
 //📌action
 //as const 를 사용하면 action.type이 string으로 추론되지않고, 문자열로 추론된다!!
 const INCREASE = "counter/INCREASE" as const;
 const DECREASE = "counter/DECREASE" as const;
 const INCREASE_BY = "counter/INCREASE_BY" as const;
 
-export const increase = () => ({
-    type: INCREASE,
-});
+export const increase = createStandardAction(INCREASE)();
 
-export const decrease = () => ({
-    type: DECREASE,
-});
+export const decrease = createStandardAction(DECREASE)();
 
-export const increaseBy = (diff: number) => ({
-    type: INCREASE_BY,
-    payload: diff,
-});
+//payload에 있는 number값을 원본 state에 더하는 역할
+//increaseBy에서 사용할 payload의 type을 generics로 지정한다.
+export const increaseBy = createStandardAction(INCREASE_BY)<number>();
 
 //📌action type
-// ReturnType<typeof _____> 는 특정 함수의 반환값을 추론해줍니다
-type CounterAction =
-    | ReturnType<typeof increase>
-    | ReturnType<typeof decrease>
-    | ReturnType<typeof increaseBy>;
+const actions = { increase, decrease, increaseBy };
+type CounterAction = ActionType<typeof actions>; //ActionType을 이용하여 모든 액션 객체들의 타입 선언
 
 //📌state type
 type CounterState = {
@@ -35,21 +30,10 @@ const initialState: CounterState = {
 };
 
 //📌reducer
-function counter(
-    state: CounterState = initialState,
-    action: CounterAction
-): CounterState {
-    switch (action.type) {
-        //case를 적고, ctrl+space를 하면 action 선언시 as const로 설정한 action들이 자동으로 뜬다.
-        case "counter/INCREASE":
-            return { count: state.count + 1 };
-        case "counter/DECREASE":
-            return { count: state.count - 1 };
-        case "counter/INCREASE_BY":
-            return { count: state.count + action.payload };
-        default:
-            return state;
-    }
-}
+const counter = createReducer<CounterState, CounterAction>(initialState, {
+    [INCREASE]: state => ({ count: state.count + 1 }),
+    [DECREASE]: state => ({ count: state.count - 1 }),
+    [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload }),
+});
 
 export default counter;
